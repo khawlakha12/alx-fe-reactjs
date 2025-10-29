fafc3a40a7872f0f36dfa88778d389861f07b2ab
Khawlita_kha <144932679+khawlakha12@users.noreply.github.com>
2025-10-29 19:29:39 +0100

    header

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,16 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
-    <div>
-        <WelcomeMessage />
-    </div>
-);
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
+
+
