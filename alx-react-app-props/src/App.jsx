feccac416feff8d6e1da544b1c3772ec308023a2
Khawlita_kha <144932679+khawlakha12@users.noreply.github.com>
2025-11-05 18:35:31 +0100

    modif Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,17 +7,15 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
+import UserInfo from './components/UserInfo.jsx';
+import UserDetails from './components/UserDetails.jsx';
+import UserContext from './components/UserContext.jsx';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+  return <ProfilePage userData={userData} />;
 }
 
 export default App;
--- alx-react-app-props/src/components/UserInfo.jsx
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import UserDetails from './UserDetails';
 
-function UserInfo() {
-  return <UserDetails />;
+function UserInfo({ userData }) {
+  return <UserDetails userData={userData} />;
 }
 
 export default UserInfo;
