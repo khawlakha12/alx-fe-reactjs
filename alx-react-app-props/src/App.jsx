2d2dcd3cf4730d85ba83ff686ac830705af869da
Khawlita_kha <144932679+khawlakha12@users.noreply.github.com>
2025-11-05 18:47:59 +0100

    modif app

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,23 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import './App.css';
 import ProfilePage from './components/ProfilePage';
-import UserInfo from './components/UserInfo.jsx';
-import UserDetails from './components/UserDetails.jsx';
-import UserContext from './components/UserContext.jsx';
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return (
+
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
-
-
