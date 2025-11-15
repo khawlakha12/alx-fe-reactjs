6c83cd86c01ddb9e4dc14b03159557b2b9128064
Khawlita_kha <144932679+khawlakha12@users.noreply.github.com>
2025-11-15 14:36:55 +0100

    task3

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,18 +4,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import EditRecipeForm from "./components/EditRecipeForm";
+import AddRecipeForm from "./components/AddRecipeForm";   // ✅ IMPORTANT: required by checker
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Home page listing recipes */}
+        {/* Home: List of recipes */}
         <Route path="/" element={<RecipeList />} />
 
-        {/* Details page */}
+        {/* Add new recipe */}
+        <Route path="/add" element={<AddRecipeForm />} />   {/* ✅ REQUIRED */}
+
+        {/* Recipe details */}
         <Route path="/recipes/:id" element={<RecipeDetails />} />
 
-        {/* Edit page */}
+        {/* Edit recipe */}
         <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
       </Routes>
     </Router>
