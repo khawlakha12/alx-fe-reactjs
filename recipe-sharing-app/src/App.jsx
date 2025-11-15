85dc99ce1b7f63effbc44c7adbf2b749837f78c4
Khawlita_kha <144932679+khawlakha12@users.noreply.github.com>
2025-11-15 14:31:06 +0100

    update task 2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import RecipeList from './components/RecipeList'
-import RecipeDetails from './components/RecipeDetails'
-import EditRecipeForm from './components/EditRecipeForm'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
 
-export default function App() {
-return (
-<div style={{ padding: 20, fontFamily: 'Arial, sans-serif' }}>
-<header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-<h2>Recipe Sharing App</h2>
-<nav>
-<Link to="/" style={{ marginRight: 12 }}>Home</Link>
-<Link to="/add">Add</Link>
-</nav>
-</header>
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Home page listing recipes */}
+        <Route path="/" element={<RecipeList />} />
 
+        {/* Details page */}
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
 
-<main style={{ marginTop: 20 }}>
-<Routes>
-<Route path="/" element={<RecipeList />} />
-<Route path="/recipes/:id" element={<RecipeDetails />} />
-<Route path="/edit/:id" element={<EditRecipeForm />} />
-{/* Optional Add route can reuse EditRecipeForm with no id */}
-<Route path="/add" element={<EditRecipeForm />} />
-</Routes>
-</main>
-</div>
-)
-}
+        {/* Edit page */}
+        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
