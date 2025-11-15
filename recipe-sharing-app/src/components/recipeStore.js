1d0a5240e8a7238f2707d02ac91c6648eea7ab76
Khawlita_kha <144932679+khawlakha12@users.noreply.github.com>
2025-11-15 14:03:57 +0100

    update task 2

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,35 +1,22 @@
-import create from 'zustand'
+import create from "zustand";
 
+export const useRecipeStore = create((set) => ({
+  recipes: [],
 
-export const useRecipeStore = create((set, get) => ({
-recipes: [
-{
-id: '1',
-title: 'Spaghetti Aglio e Olio',
-description: 'Simple garlic and olive oil spaghetti.',
-ingredients: ['spaghetti', 'garlic', 'olive oil', 'red pepper flakes', 'parsley'],
-steps: ['Boil pasta', 'Sauté garlic', 'Toss together', 'Serve']
-},
-{
-id: '2',
-title: 'Tomato Basil Soup',
-description: 'Comforting tomato soup with fresh basil.',
-ingredients: ['tomatoes', 'onion', 'garlic', 'basil', 'cream'],
-steps: ['Roast tomatoes', 'Blend', 'Simmer', 'Serve']
-}
-],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
 
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 
-addRecipe: (recipe) =>
-set((state) => ({ recipes: [...state.recipes, recipe] })),
-
-
-deleteRecipe: (id) =>
-set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
-
-
-updateRecipe: (updatedRecipe) =>
-set((state) => ({
-recipes: state.recipes.map((r) => (r.id === updatedRecipe.id ? { ...r, ...updatedRecipe } : r))
-}))
-}))
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+}));
