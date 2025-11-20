f43960f96d5f49cce5d05dd8b8460fccbe07c725
khawlakha12 <kharbouchikhawla603@gmail.com>
2025-11-20 16:04:31 +0100

    MODIF API task 3

---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/index.css",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
